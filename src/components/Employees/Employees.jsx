2abc4f548443c7f1389e0b05999bc1f809006da7
Agata Rojewska <agata.rojewska@gmail.com>
2023-12-19 17:58:55 +0100

    My changes, custom hook

--- src/components/Employees/Employees.jsx
+++ src/components/Employees/Employees.jsx
@@ -1,63 +1,35 @@
-import { useEffect, useState } from "react";
-import styles from "./Employees.module.css";
+import { useGetData } from "../../hooks/useGetData.js"
+import styles from "./Employees.module.css"
 
 export function Employees() {
-    const [employees, setEmployees] = useState([]);
-    const [error, setError] = useState(null);
+  const { data: employees, error } = useGetData("/employees.json")
 
-    useEffect(() => {
-        let isCancelled = false;
-
-        fetch("/employees.json")
-            .then((res) => {
-                if (res.ok) {
-                    setError(null);
-                    return res.json();
-                }
-
-                throw new Error("Coś poszło nie tak...");
-            })
-            .then((res) => {
-                if (isCancelled) {
-                    return;
-                }
-                setEmployees(res);
-            })
-            .catch((e) => {
-                setError(e);
-            });
-
-        return () => {
-            isCancelled = true;
-        };
-    }, []);
-
-    return (
-        <section className={styles.section}>
-            {error ? (
-                error.message
-            ) : (
-                <table className={styles.tableContainer}>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Salary</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {employees.map((employee) => (
-                            <tr key={employee.id}>
-                                <td>{employee.id}</td>
-                                <td>
-                                    {employee.name} {employee.last_name}
-                                </td>
-                                <td>{employee.salary}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            )}
-        </section>
-    );
+  return (
+    <section className={styles.section}>
+      {error ? (
+        error.message
+      ) : (
+        <table className={styles.tableContainer}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Salary</th>
+            </tr>
+          </thead>
+          <tbody>
+            {employees.map(employee => (
+              <tr key={employee.id}>
+                <td>{employee.id}</td>
+                <td>
+                  {employee.name} {employee.last_name}
+                </td>
+                <td>{employee.salary}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </section>
+  )
 }
